fdff4f95351122c1c17da394028086c1257c7182
Aaron Pina <aaron.pina@gmail.com>
2026-04-12 11:47:34 +0200

    Added auth code to RedisKeys.

--- Aaron.Pina.Blog.Article.13.Server/RedisKeys.cs
+++ Aaron.Pina.Blog.Article.13.Server/RedisKeys.cs
@@ -5,8 +5,11 @@ public static class RedisKeys
     private const string Root = "jwt-server";
     
     public static string Blacklist(string jti)  => $"{Root}:blacklist:{jti}";
-    public static string CurrentKid()           => $"{Root}:jwks:current-kid";   // points to the active signing key
-    public static string PrivateKey(string kid) => $"{Root}:jwks:private:{kid}"; // full private material (short life)
-    public static string PublicKey(string kid)  => $"{Root}:jwks:public:{kid}";  // public material only (long life)
-    public static string KeyHistory()           => $"{Root}:jwks:key-history";   // simple list of every KID we have ever used
+    
+    public static string AuthCode(string code)  => $"{Root}:auth:code:{code}";
+
+    public static string CurrentKid()           => $"{Root}:jwks:current-kid";
+    public static string PrivateKey(string kid) => $"{Root}:jwks:private:{kid}";
+    public static string PublicKey(string kid)  => $"{Root}:jwks:public:{kid}";
+    public static string KeyHistory()           => $"{Root}:jwks:key-history";
 }
