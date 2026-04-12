119bcc8032ec916dfe5731b3d30a1a64501f72f8
Aaron Pina <aaron.pina@gmail.com>
2026-04-12 11:57:12 +0200

    Added AuthCode record to represent authorization code details.

---
+++ Aaron.Pina.Blog.Article.13.Server/AuthCode.cs
@@ -0,0 +1,12 @@
+namespace Aaron.Pina.Blog.Article._13.Server;
+
+public record AuthCode
+{
+    public string   ClientId            { get; init; } = string.Empty;
+    public string   Subject             { get; init; } = string.Empty;
+    public string   RedirectUri         { get; init; } = string.Empty;
+    public string   CodeChallenge       { get; init; } = string.Empty;
+    public string   CodeChallengeMethod { get; init; } = "S256";
+    public string[] Scopes              { get; init; } = [];
+    public DateTime ExpiresAt           { get; init; }
+}
